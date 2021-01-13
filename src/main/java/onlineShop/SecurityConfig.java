425a7d56ab3bef110dfa68c5690b8cbfb19b3597
zyihong <zyihong@umich.edu>
2021-01-13 18:51:35 -0500

    authorize & authenication

---
+++ src/main/java/onlineShop/SecurityConfig.java
@@ -0,0 +1,52 @@
+package onlineShop;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+import javax.sql.DataSource;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .formLogin()
+                .loginPage("/login");
+
+        http
+                .authorizeRequests()
+                .antMatchers("/cart/**").hasAuthority("ROLE_USER")
+                .antMatchers("/get*/**").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
+                .antMatchers("/admin*/**").hasAuthority("ROLE_ADMIN")
+                .anyRequest().permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("admin@gmail.com")
+                .password("admin").authorities("ROLE_ADMIN");
+
+        auth
+                .jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("SELECT emailId, password, enabled FROM users WHERE emailId=?")
+                .authoritiesByUsernameQuery("SELECT emailId, authorities FROM authorities WHERE emailId=?");
+    }
+
+    @SuppressWarnings("deprecation")
+    @Bean
+    public static NoOpPasswordEncoder passwordEncoder() {
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    }
+}
