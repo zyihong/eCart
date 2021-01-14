7250abbde945b9795c83871080e12aa898a123ba
zyihong <zyihong@umich.edu>
2021-01-13 20:51:17 -0500

    cart controller/service/dao

---
+++ src/main/java/onlineShop/dao/CartDao.java
@@ -0,0 +1,27 @@
+package onlineShop.dao;
+
+import java.io.IOException;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import onlineShop.entity.Cart;
+import onlineShop.entity.CartItem;
+
+@Repository
+public class CartDao {
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    public Cart getCartById(int cartId) {
+        Cart cart = null;
+        try (Session session = sessionFactory.openSession()) {
+            cart = session.get(Cart.class, cartId);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return cart;
+    }
+}
